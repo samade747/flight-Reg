e83bdbb8f67cbe9d5d8027d3b27950fcbd9434e1
samade747 <samad.e747@gmail.com>
2023-12-22 15:50:41 +0500

    22 12 2023 updating data base

---
+++ app.js
@@ -0,0 +1,44 @@
+const passortDataBase = [
+    {}
+]
+
+
+
+
+function flightDataInput(){
+let flightNum = document.getElementById('flightNum');
+let passengerName = document.getElementById('TicketName');
+let passportNumber = document.getElementById('passportNumber');
+
+let flightDetails = {
+    passengerName: passengerName.value,
+    flightNumber: flightNum.value,
+    passportNumber: passportNumber.value
+}
+
+
+
+}
+
+
+
+
+
+const checkIn = function (flightNumber, passenger) {
+    // let flightNum = document.getElementById('flightNum');
+    // console.log(flightNum.value);
+    // flightNum.value = '';
+    if(passportNumber === passortDataBase)
+
+    0
+
+
+    
+
+
+    
+
+    
+}
+
+
